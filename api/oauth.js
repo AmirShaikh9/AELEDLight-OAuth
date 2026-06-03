f3572cc56b43c8c486f23dfb628dc60338cfbfae
Amir <amirshaikh92112@gmail.com>
2026-06-03 16:58:01 +0500

    fix: serve HTML popup page that performs two-way handshake for Decap CMS OAuth

--- api/oauth.js
+++ api/oauth.js
@@ -2,6 +2,11 @@ const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 const TOKEN_URL = "https://github.com/login/oauth/access_token";
 
+function serveHtml(res, html) {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.status(200).send(html);
+}
+
 module.exports = async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const code = url.searchParams.get("code");
@@ -17,13 +22,39 @@ module.exports = async (req, res) => {
       if (!data.access_token) {
         return res.status(400).json(data);
       }
-      return res.redirect(`https://aeledlight.web.app/admin#access_token=${data.access_token}`);
+
+      const token = JSON.stringify(data.access_token);
+      serveHtml(res, `<!DOCTYPE html>
+<html><body><script>
+(function(){
+  var t = ${token};
+  window.opener.postMessage("authorizing:github","*");
+  function h(e){
+    if(e.data==="authorizing:github"){
+      window.removeEventListener("message",h);
+      window.opener.postMessage(
+        "authorization:github:success:"+JSON.stringify({token:t,provider:"github"}),
+        e.origin
+      );
+      window.close();
+    }
+  }
+  window.addEventListener("message",h);
+  setTimeout(function(){
+    try{window.opener.postMessage(
+      "authorization:github:success:"+JSON.stringify({token:t,provider:"github"}),
+      "*"
+    );window.close()}catch(e){}
+  },8000);
+})();
+<\/script></body></html>`);
     } catch (err) {
       return res.status(500).send(err.message);
     }
+    return;
   }
 
-  const redirectUri = `https://aeled-light-o-auth-1i1u.vercel.app/api/oauth`;
+  const redirectUri = "https://aeled-light-o-auth-1i1u.vercel.app/api/oauth";
   const authUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=repo&response_type=code`;
   res.redirect(authUrl);
 };
