8561fee2a9b3bef4222523ef49cad0b1934a62b9
Amir <amirshaikh92112@gmail.com>
2026-06-03 15:18:32 +0500

    switch to single oauth.js using CommonJS

--- api/auth.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-  const SITE_URL = "https://aeled-light-o-auth-1i1u.vercel.app";
-  const redirectUri = `${SITE_URL}/api/auth/callback`;
-
-  const url = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=repo&response_type=code`;
-  res.redirect(url);
-}
