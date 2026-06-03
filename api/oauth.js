98ba3c7e4ad4b2443a4eb78afab4be69e47eaacc
Amir <amirshaikh92112@gmail.com>
2026-06-03 17:10:15 +0500

    fix: add visible debug output to OAuth popup page and use writeHead/end

--- api/oauth.js
+++ api/oauth.js
@@ -2,13 +2,8 @@ const CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 const TOKEN_URL = "https://github.com/login/oauth/access_token";
 
-function serveHtml(res, html) {
-  res.setHeader("Content-Type", "text/html; charset=utf-8");
-  res.status(200).send(html);
-}
-
 module.exports = async (req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
+  const url = new URL(req.url, "http://" + req.headers.host);
   const code = url.searchParams.get("code");
 
   if (code) {
@@ -20,41 +15,45 @@ module.exports = async (req, res) => {
       });
       const data = await r.json();
       if (!data.access_token) {
-        return res.status(400).json(data);
+        res.writeHead(400, { "Content-Type": "application/json" });
+        return res.end(JSON.stringify(data));
       }
 
-      const token = JSON.stringify(data.access_token);
-      serveHtml(res, `<!DOCTYPE html>
-<html><body><script>
-(function(){
-  var t = ${token};
-  window.opener.postMessage("authorizing:github","*");
-  function h(e){
-    if(e.data==="authorizing:github"){
-      window.removeEventListener("message",h);
-      window.opener.postMessage(
-        "authorization:github:success:"+JSON.stringify({token:t,provider:"github"}),
-        e.origin
-      );
-      window.close();
-    }
-  }
-  window.addEventListener("message",h);
-  setTimeout(function(){
-    try{window.opener.postMessage(
-      "authorization:github:success:"+JSON.stringify({token:t,provider:"github"}),
-      "*"
-    );window.close()}catch(e){}
-  },8000);
-})();
-<\/script></body></html>`);
+      var token = JSON.stringify(data.access_token);
+      var html = '<!DOCTYPE html><html><head><meta charset="utf-8"><title>Auth</title></head><body>';
+      html += '<p id="m">Authenticating...</p>';
+      html += '<script>var m=document.getElementById("m");var t=' + token + ';';
+      html += 'try{';
+      html += 'm.textContent="Sending handshake...";';
+      html += 'window.opener.postMessage("authorizing:github","*");';
+      html += 'm.textContent="Waiting for reply...";';
+      html += 'function h(e){if(e.data==="authorizing:github"){';
+      html += 'window.removeEventListener("message",h);';
+      html += 'm.textContent="Sending token...";';
+      html += 'window.opener.postMessage("authorization:github:success:"+JSON.stringify({token:t,provider:"github"}),e.origin);';
+      html += 'm.textContent="Done! Closing...";';
+      html += 'setTimeout(function(){window.close()},300);';
+      html += '}}';
+      html += 'window.addEventListener("message",h);';
+      html += 'setTimeout(function(){try{';
+      html += 'window.opener.postMessage("authorization:github:success:"+JSON.stringify({token:t,provider:"github"}),"*");';
+      html += 'm.textContent="Token sent, closing...";';
+      html += 'setTimeout(function(){window.close()},500);';
+      html += '}catch(e){m.textContent="Error: "+e.message}}';
+      html += ',5000);';
+      html += '}catch(e){m.textContent="Error: "+e.message}';
+      html += '<\/script></body></html>';
+
+      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      res.end(html);
     } catch (err) {
-      return res.status(500).send(err.message);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end(err.message);
     }
     return;
   }
 
-  const redirectUri = "https://aeled-light-o-auth-1i1u.vercel.app/api/oauth";
-  const authUrl = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=repo&response_type=code`;
+  var redirectUri = "https://aeled-light-o-auth-1i1u.vercel.app/api/oauth";
+  var authUrl = "https://github.com/login/oauth/authorize?client_id=" + encodeURIComponent(CLIENT_ID) + "&redirect_uri=" + encodeURIComponent(redirectUri) + "&scope=repo&response_type=code";
   res.redirect(authUrl);
 };
